793e5cc5302fad10e84235910ce7a19d0e769723
scisearch-ai <jean.p.ferreira96@gmail.com>
2025-04-12 23:53:21 -0300

    Update main.py

--- scisearch-ai_plus_final_ready/main.py
+++ scisearch-ai_plus_final_ready/main.py
@@ -1,5 +1,9 @@
 # main.py
-from app.routes import app
+from flask import Flask
+from app.routes import app as routes_blueprint
+
+app = Flask(__name__, template_folder="app/templates")
+app.register_blueprint(routes_blueprint)
 
 if __name__ == "__main__":
     app.run(debug=True)
